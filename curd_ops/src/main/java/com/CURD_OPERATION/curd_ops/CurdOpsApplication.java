f5ff9e62cab08d91c75701893337f2f7439f03eb
Dharmendra <Dharmendra@DESKTOP-KH0MIJS>
2019-08-15 22:39:21 -0700

    test

---
+++ curd_ops/src/main/java/com/CURD_OPERATION/curd_ops/CurdOpsApplication.java
@@ -0,0 +1,13 @@
+package com.CURD_OPERATION.curd_ops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurdOpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurdOpsApplication.class, args);
+	}
+
+}
