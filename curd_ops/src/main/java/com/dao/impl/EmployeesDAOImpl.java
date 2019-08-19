84990042582f8157949ecf81f3e65ab64335f160
Dharmendra <Dharmendra@DESKTOP-KH0MIJS>
2019-08-19 04:30:46 -0700

    new commit...

--- curd_ops/src/main/java/com/dao/impl/EmployeesDAOImpl.java
+++ curd_ops/src/main/java/com/dao/impl/EmployeesDAOImpl.java
@@ -1,48 +1,38 @@
 package com.dao.impl;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import com.dao.EmployeesDAO;
 import com.entity.Employees;
 
-
+@Repository
 public class EmployeesDAOImpl implements EmployeesDAO{
+	
+	@Autowired
+	private SessionFactory sessionFactory;
 
 	@Override
 	public void save(Employees emp) {
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/employees","root","root");
-			Statement smts = con.createStatement();
-			smts.executeUpdate("INSERT INTO employees(emp_no,birth_date,first_name,last_name,gender,hire_date) VALUES('"+emp.getEmp_no()+"','"+emp.getBirth_date()+"','"+emp.getFirst_name()+"','"+emp.getLast_name()+"','"+emp.getGender()+"','"+emp.getHire_date()+"')");
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		this.sessionFactory.getCurrentSession().save(emp);
 	}
 
 	@Override
 	public Employees fetch(Employees emp) {
-		try {	
-			Class.forName("com.mysql.jdbc.Driver");
-			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/employees","root","root");
-			
-			Statement smts = con.createStatement();
-			ResultSet rs = smts.executeQuery("SELECT * FROM employees WHERE dept_no =  '"+emp.getEmp_no()+"'" );
-			while(rs.next()) { 
-				emp.setEmp_no(rs.getInt(1));
-				emp.setBirth_date(rs.getDate(1));
-				emp.setFirst_name(rs.getString(3));
-				emp.setLast_name(rs.getString(4));
-				emp.setGender(rs.getNString(5));
-				emp.setHire_date(rs.getDate(6));
-			} 
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		
 		return emp;
+	}
+
+	@Override
+	public Employees delete(Employees emp) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Employees update(Employees emp) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
--- curd_ops/src/main/java/com/entity/Employees.java
+++ curd_ops/src/main/java/com/entity/Employees.java
@@ -13,54 +13,71 @@ import javax.persistence.Table;
 public class Employees {
 	
 	@Id
-	@Column(name="emp_no")
+	@Column(name="emp_no", nullable = false)
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int emp_no;
+	
+	@Column(name="birth_date", nullable = false)
 	private Date birth_date;
+	
+	@Column(name="first_name" , nullable = false)
 	private String first_name;
+	
+	@Column(name="last_name" , nullable = false)
 	private String last_name;
+	
+	@Column(name="gender" , nullable = false)
 	private String gender;
+	
+	@Column(name="hire_date" , nullable = false)
 	private Date hire_date;
 	
 	public int getEmp_no() {
 		return emp_no;
 	}
+	
 	public void setEmp_no(int emp_no) {
 		this.emp_no = emp_no;
 	}
+	
 	public Date getBirth_date() {
 		return birth_date;
 	}
+	
 	public void setBirth_date(Date birth_date) {
 		this.birth_date = birth_date;
 	}
+	
 	public String getFirst_name() {
 		return first_name;
 	}
+	
 	public void setFirst_name(String first_name) {
 		this.first_name = first_name;
 	}
+	
 	public String getLast_name() {
 		return last_name;
 	}
+	
 	public void setLast_name(String last_name) {
 		this.last_name = last_name;
 	}
+	
 	public String getGender() {
 		return gender;
 	}
+	
 	public void setGender(String gender) {
 		this.gender = gender;
 	}
+	
 	public Date getHire_date() {
 		return hire_date;
 	}
+	
 	public void setHire_date(Date hire_date) {
 		this.hire_date = hire_date;
 	}
-	@Override
-	public String toString() {
-		return "Employees [emp_no=" + emp_no + ", birth_date=" + birth_date + ", first_name=" + first_name
-				+ ", last_name=" + last_name + ", gender=" + gender + ", hire_date=" + hire_date + "]";
-	}
+	
 }
