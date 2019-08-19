94f16bd7385192bbf4bcdef5514929906f6cffb1
Dharmendra <Dharmendra@DESKTOP-KH0MIJS>
2019-08-18 23:16:45 -0700

    Revert "Dao File and pom file"
    This reverts commit 99bfba256e1c96855f7f5eb301da9a500ed55268.

--- curd_ops/src/main/java/com/dao/impl/DepartmentDAOImpl.java
+++ curd_ops/src/main/java/com/dao/impl/DepartmentDAOImpl.java
@@ -1,10 +1,5 @@
 package com.dao.impl;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
-
 import com.dao.DepartmentDAO;
 import com.entity.Department;
 
@@ -12,33 +7,14 @@ public class DepartmentDAOImpl  implements DepartmentDAO{
 
 	@Override
 	public void save(Department dept) {
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/department","root","root");
-			Statement smts = con.createStatement();
-			smts.executeUpdate("INSERT INTO department(dept_no,dept_name) VALUES('"+dept.getDept_no()+"','"+dept.getDept_name()+"')");
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		// TODO Auto-generated method stub
+		
 	}
 
 	@Override
 	public Department fetch(Department dept) {
-		try {	
-			Class.forName("com.mysql.jdbc.Driver");
-			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/department","root","root");
-			
-			Statement smts = con.createStatement();
-			ResultSet rs = smts.executeQuery("SELECT * FROM department WHERE dept_no =  '"+dept.getDept_no()+"'" );
-			while(rs.next()) { 
-				dept.setDept_no(rs.getString(1));
-				dept.setDept_name(rs.getString(2));
-			} 
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return dept;
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
--- curd_ops/src/main/java/com/dao/impl/EmployeesDAOImpl.java
+++ curd_ops/src/main/java/com/dao/impl/EmployeesDAOImpl.java
@@ -1,48 +1,18 @@
 package com.dao.impl;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
-
 import com.dao.EmployeesDAO;
 import com.entity.Employees;
-
 
 public class EmployeesDAOImpl implements EmployeesDAO{
 
 	@Override
 	public void save(Employees emp) {
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/employees","root","root");
-			Statement smts = con.createStatement();
-			smts.executeUpdate("INSERT INTO employees(emp_no,birth_date,first_name,last_name,gender,hire_date) VALUES('"+emp.getEmp_no()+"','"+emp.getBirth_date()+"','"+emp.getFirst_name()+"','"+emp.getLast_name()+"','"+emp.getGender()+"','"+emp.getHire_date()+"')");
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		// TODO Auto-generated method stub
 	}
 
 	@Override
 	public Employees fetch(Employees emp) {
-		try {	
-			Class.forName("com.mysql.jdbc.Driver");
-			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/employees","root","root");
-			
-			Statement smts = con.createStatement();
-			ResultSet rs = smts.executeQuery("SELECT * FROM employees WHERE dept_no =  '"+emp.getEmp_no()+"'" );
-			while(rs.next()) { 
-				emp.setEmp_no(rs.getInt(1));
-				emp.setBirth_date(rs.getDate(1));
-				emp.setFirst_name(rs.getString(3));
-				emp.setLast_name(rs.getString(4));
-				emp.setGender(rs.getNString(5));
-				emp.setHire_date(rs.getDate(6));
-			} 
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return emp;
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
